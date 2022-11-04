bde8ad5f3662b833b3399104a6c823e65933ca35
trigaten <sanderschulhoff@gmail.com>
2022-11-04 09:46:07 -0400

    style: black

--- src/proof_data_analysis/plots.py
+++ src/proof_data_analysis/plots.py
@@ -1,9 +1,10 @@
+from collections import Counter
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from collections import Counter
 
-def plot_edits_over_time(df:pd.DataFrame):
+def plot_edits_over_time(df: pd.DataFrame):
     """Plot the number of edits over time"""
     # just plotting time against the index
     plt.plot(df["Time"], list(df.index))
@@ -12,26 +13,27 @@ def plot_edits_over_time(df:pd.DataFrame):
     plt.ylabel("Total Number of Edits")
     plt.title("Edits Over Time")
 
-def plot_letter_count(df:pd.DataFrame) -> None:
+
+def plot_letter_count(df: pd.DataFrame) -> None:
     # from https://stackoverflow.com/questions/26520111/how-can-i-convert-special-characters-in-a-string-back-into-escape-sequences
     def raw(string: str, replace: bool = False) -> str:
         """Returns the raw representation of a string. If replace is true, replace a single backslash's repr \\ with \."""
         r = repr(string)[1:-1]  # Strip the quotes from representation
         if replace:
-            r = r.replace('\\\\', '\\')
+            r = r.replace("\\\\", "\\")
         return r
 
     letter_counter = Counter()
     for i, row in df.iterrows():
         letter_counter[raw(row["Text_Change"])] += 1
 
-    empty = letter_counter.pop(' ')
+    empty = letter_counter.pop(" ")
     letter_counter[repr("")] = empty
 
     # from https://stackoverflow.com/questions/16010869/plot-a-bar-using-matplotlib-using-a-dictionary
     D = letter_counter
 
-    p = plt.bar(range(len(D)), list(D.values()), align='center')
+    p = plt.bar(range(len(D)), list(D.values()), align="center")
     plt.xticks(range(len(D)), list(D.keys()))
 
     plt.xlabel("Letter")
--- src/proof_data_analysis/statistics.py
+++ src/proof_data_analysis/statistics.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-def calc_statistics(df:pd.DataFrame) -> None:
+
+def calc_statistics(df: pd.DataFrame) -> None:
     print("Total number of edits: {}".format(len(df)))
