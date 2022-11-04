3700b9775c89fbee984bd5a51fbe1422ca98f4b7
trigaten <sanderschulhoff@gmail.com>
2022-11-04 09:53:12 -0400

    test: test graph funcs

--- tests/test_plot_timed_edits.py
+++ tests/test_plot_timed_edits.py
@@ -1,7 +1,10 @@
-from utils import load_df
+from proof_data_analysis.utils import load_df
+from proof_data_analysis.plots import plot_edits_over_time, plot_letter_count
 
 
 class TestPlotTimedEdits:
     def test_plot_timed_edits(self):
-        df = load_df()
-        # plot_timed_edits(df)
+        """Just test that the function runs without error"""
+        df = load_df("tests/example.json")
+        plot_edits_over_time(df)
+        plot_letter_count(df)
