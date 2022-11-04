533cfa98037ea84feae4b2f269b070f2cc3cd309
trigaten <sanderschulhoff@gmail.com>
2022-11-04 09:48:19 -0400

    docs: docstrings

--- src/proof_data_analysis/plots.py
+++ src/proof_data_analysis/plots.py
@@ -13,6 +13,8 @@ def plot_edits_over_time(df:pd.DataFrame) -> None:
     plt.title("Edits Over Time")
 
 def plot_letter_count(df:pd.DataFrame) -> None:
+    """Plot a bar graph of the number of times each letter was typed"""
+
     # from https://stackoverflow.com/questions/26520111/how-can-i-convert-special-characters-in-a-string-back-into-escape-sequences
     def raw(string: str, replace: bool = False) -> str:
         """Returns the raw representation of a string. If replace is true, replace a single backslash's repr \\ with \."""
@@ -21,13 +23,19 @@ def plot_letter_count(df:pd.DataFrame) -> None:
             r = r.replace('\\\\', '\\')
         return r
 
+    # use Counter to count the number of times each letter is typed
     letter_counter = Counter()
-    for i, row in df.iterrows():
+    # iterate through each row in the dataframe to count 
+    # the number of times each letter is typed
+    for _, row in df.iterrows():
+        # get raw representation of the text, that \n and \t are not escaped
         letter_counter[raw(row["Text_Change"])] += 1
 
+    # deal with special case of space
     empty = letter_counter.pop(' ')
     letter_counter[repr("")] = empty
 
+    # plot the bar graph
     # from https://stackoverflow.com/questions/16010869/plot-a-bar-using-matplotlib-using-a-dictionary
     D = letter_counter
 
