3be4f83472e86a036e6fe85275ef1335e8904060
Nathan Young <nathanyoung@rccs.org>
2022-11-17 00:57:51 -0500

    fix: stop thread on window close

--- src/proof_data_analysis/replay.py
+++ src/proof_data_analysis/replay.py
@@ -8,16 +8,18 @@ from functools import partial
 class Replay:
     FRAME_TIME = 0.01
     SECONDS_TO_MILLISECONDS = 1000
-    PAUSE_TIMEOUT = 10
+    PAUSE_TIMEOUT = 1
     playback_speed = 1
-    is_playing = threading.Event();
-    start_time = 0;
-    curr_event = 0;
-    curr_time = 0;
-    displayed_event = 0;
-    displayed_time = 0;
-    displayed_text = {};
-    events = [];
+    is_playing = threading.Event()
+    window_open = threading.Event()
+    start_time = 0
+    end_time = 0
+    curr_event = 0
+    curr_time = 0
+    displayed_event = 0
+    displayed_time = 0
+    displayed_text = {}
+    events = []
 
     def set_speed(self, speed: float):
         """Sets the speed of the playback. Speed is the coefficient of time, with 1 being normal speed,
@@ -25,12 +27,12 @@ class Replay:
         playback_speed = speed
 
 
-    def next_text_event(self, events: list[dict], start_index: int):
+    def next_text_event(self, start_index: int):
         """Starts searching the events array at start index and returns the index of the next text event
         (including the start index if it is a text event). Returns an index out of bounds of the array
         if there is no text event left in the array."""
         curr_index = start_index
-        while curr_index < len(events) and "textChange" not in events[curr_index]:
+        while curr_index < len(self.events) and "textChange" not in self.events[curr_index]:
             curr_index += 1
 
         return curr_index
@@ -45,12 +47,12 @@ class Replay:
             self.displayed_text.insert(location, event["textChange"])
 
     def rewind_to_time(self, time: int):
-        event = self.next_text_event(self.events, 0)
-        next_event = self.next_text_event(self.events, event + 1)
+        event = self.next_text_event(0)
+        next_event = self.next_text_event(event + 1)
         time_absolute = time + self.start_time
         while(next_event < len(self.events) and self.events[next_event]["time"] <= time_absolute):
             event = next_event
-            next_event = self.next_text_event(self.events, next_event + 1)
+            next_event = self.next_text_event(next_event + 1)
 
         if self.events[event]["time"] > time_absolute:
             self.displayed_text.delete('1.0', 'end')
@@ -60,17 +62,31 @@ class Replay:
 
     def revert_to_event(self, event: dict):
         """Rebuilds the current state up to the specified event"""
-        self.curr_event = self.next_text_event(self.events, 0)
+        self.curr_event = self.next_text_event(0)
         self.curr_time = int(event["time"])
         self.displayed_text.delete('1.0', 'end')
         while (
                     self.curr_event < len(self.events) and self.events[self.curr_event]["time"] <= self.curr_time
                 ):
                     self.apply_text_event(self.events[self.curr_event])
-                    self.curr_event = self.next_text_event(self.events, self.curr_event + 1)
+                    self.curr_event = self.next_text_event(self.curr_event + 1)
 
-    def scrub_to_event(self, event):
+    def scrub_to_event(self, event: str):
         self.is_playing.clear()
+        delta_event = int(event) - self.curr_event
+        i = self.curr_event
+        self.curr_event += delta_event
+        if (delta_event > 0):
+            while (i < self.curr_event):
+                i = self.next_text_event(i)
+                if i < len(self.events):
+                    self.apply_text_event(self.events[i])
+                i += 1
+
+        elif (delta_event < 0):
+            next_index = self.next_text_event(int(event))
+            if next_index < len(self.events):
+                self.revert_to_event(self.events[next_index])
 
     def scrub_to_time(self, time: str):
         self.is_playing.clear()
@@ -81,7 +97,7 @@ class Replay:
                     self.curr_event < len(self.events) and self.events[self.curr_event]["time"] <= self.curr_time
                 ):
                     self.apply_text_event(self.events[self.curr_event])
-                    self.curr_event = self.next_text_event(self.events, self.curr_event + 1)
+                    self.curr_event = self.next_text_event(self.curr_event + 1)
         elif (delta_time < 0):
             self.rewind_to_time(int(time))
 
@@ -91,21 +107,22 @@ class Replay:
         and will stop playing when it is cleared."""
         self.curr_time = self.start_time
         events = data["events"]
-        while self.curr_event < len(events):
+        while self.window_open.is_set():
             if self.is_playing.wait(Replay.PAUSE_TIMEOUT):
-                time.sleep(Replay.FRAME_TIME)
-                self.curr_time += Replay.FRAME_TIME * Replay.SECONDS_TO_MILLISECONDS * self.playback_speed
-                time_label.config(text=(self.curr_time - self.start_time) / Replay.SECONDS_TO_MILLISECONDS)
-                self.displayed_time.set(self.curr_time - self.start_time)
+                if self.curr_time < self.end_time:
+                    time.sleep(Replay.FRAME_TIME)
+                    self.curr_time += Replay.FRAME_TIME * Replay.SECONDS_TO_MILLISECONDS * self.playback_speed
+                    time_label.config(text=(self.curr_time - self.start_time) / Replay.SECONDS_TO_MILLISECONDS)
+                    self.displayed_time.set(self.curr_time - self.start_time)
 
-                self.curr_event = self.next_text_event(events, self.curr_event)
-                self.displayed_event.set(self.curr_event)
+                    self.curr_event = self.next_text_event(self.curr_event)
+                    self.displayed_event.set(self.curr_event)
 
-                while (
-                    self.curr_event < len(events) and events[self.curr_event]["time"] <= self.curr_time
-                ):
-                    self.apply_text_event(events[self.curr_event])
-                    self.curr_event = self.next_text_event(events, self.curr_event + 1)
+                    while (
+                        self.curr_event < len(events) and events[self.curr_event]["time"] <= self.curr_time
+                    ):
+                        self.apply_text_event(events[self.curr_event])
+                        self.curr_event = self.next_text_event(self.curr_event + 1)
 
 
     def createWindow(self):
@@ -133,6 +150,7 @@ class Replay:
             file_data = json.loads(file.read())
             self.events = file_data["events"]
             self.start_time = int(file_data["start"])
+            self.end_time = int(file_data["end"])
             end_time = int(file_data["end"]) - self.start_time
 
             window = self.createWindow()
@@ -154,6 +172,8 @@ class Replay:
             time_label.pack()
             self.displayed_text.pack()
 
+            self.window_open.set()
+
             thread = threading.Thread(
                 target=self.startPlayback,
                 args=(
@@ -165,6 +185,7 @@ class Replay:
             )
             thread.start()
             window.mainloop()
+            self.window_open.clear()
 
 
 if __name__ == "__main__":
