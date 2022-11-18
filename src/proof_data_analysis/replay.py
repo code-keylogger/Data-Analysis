b850460d747a5aed5cfe79e02f090596fa3e822b
Nathan Young <nathanyoung@rccs.org>
2022-11-17 21:40:57 -0500

    fix: double inputs on events

--- src/proof_data_analysis/replay.py
+++ src/proof_data_analysis/replay.py
@@ -28,25 +28,16 @@ class Replay:
         2 being double speed, 0.5 being half speed and so on. Speed must be > 0."""
         self.playback_speed = speed
 
-
-    def next_text_event(self, start_index: int):
-        """Starts searching the events array at start index and returns the index of the next text event
-        (including the start index if it is a text event). Returns an index out of bounds of the array
-        if there is no text event left in the array."""
-        curr_index = start_index
-        while curr_index < len(self.events) and "textChange" not in self.events[curr_index]:
-            curr_index += 1
-
-        return curr_index
-
-
     def apply_text_event(self, event: dict):
         """Interprets the text event and applies it to the location stored in the event onto displayed_text."""
-        location = str(event["startLine"] + 1) + "." + str(event["startChar"])
+        start_location = str(event["startLine"] + 1) + "." + str(event["startChar"])
+        end_location = str(event["endLine"] + 1) + "." + str(event["endChar"])
         if event["textChange"] == "":
-            self.displayed_text.delete(location)
+            self.displayed_text.delete(start_location, end_location)
         else:
-            self.displayed_text.insert(location, event["textChange"])
+            if start_location != end_location:
+                self.displayed_text.delete(start_location, end_location)
+            self.displayed_text.insert(start_location, event["textChange"])
 
     def update_text(self):
         """updates displayed_text with any events that have occured since curr_time was increased"""
@@ -54,46 +45,48 @@ class Replay:
                     self.curr_event < len(self.events) and self.events[self.curr_event]["time"] <= self.curr_time
                 ):
                     self.apply_text_event(self.events[self.curr_event])
-                    self.curr_event = self.next_text_event(self.curr_event + 1)
+                    self.curr_event += 1;
 
     def rewind_to_time(self, time: int):
         """Reverts the state of the playback to the specified time"""
-        event = self.next_text_event(0)
-        next_event = self.next_text_event(event + 1)
+        event = 0
+        next_event = 1
         time_absolute = time + self.start_time
         while(next_event < len(self.events) and self.events[next_event]["time"] <= time_absolute):
             event = next_event
-            next_event = self.next_text_event(next_event + 1)
+            next_event += 1
 
         if self.events[event]["time"] > time_absolute:
             self.displayed_text.delete('1.0', 'end')
         else:
-            self.revert_to_event(self.events[event])
+            self.revert_to_event(event)
         
 
-    def revert_to_event(self, event: dict):
+    def revert_to_event(self, event: int):
         """Rebuilds the current state up to the specified event"""
-        self.curr_event = self.next_text_event(0)
-        self.curr_time = int(event["time"])
+        self.curr_event = 0
         self.displayed_text.delete('1.0', 'end')
-        self.update_text()
+        while self.curr_event < event:
+            self.apply_text_event(self.events[self.curr_event])
+            self.curr_event += 1
+        self.apply_text_event(self.events[self.curr_event])
+        
 
     def scrub_to_event(self, event: str):
         """Updates the playback to the event specified"""
         self.is_playing.clear()
         delta_event = int(event) - self.curr_event
-        i = self.curr_event
+        i = self.curr_event + 1
         self.curr_event += delta_event
         if (delta_event > 0):
-            while (i < self.curr_event):
-                i = self.next_text_event(i)
-                if i < len(self.events):
-                    self.apply_text_event(self.events[i])
+            while (i <= self.curr_event):
+                self.apply_text_event(self.events[i])
                 i += 1
         elif (delta_event < 0):
-            next_index = self.next_text_event(int(event))
+            next_index = int(event)
             if next_index < len(self.events):
-                self.revert_to_event(self.events[next_index])
+                self.revert_to_event(next_index)
+        self.curr_time = self.events[self.curr_event]["time"]
         self.displayed_time.set(self.curr_time - self.start_time)
 
     def scrub_to_time(self, time: str):
@@ -107,12 +100,12 @@ class Replay:
             self.rewind_to_time(int(time))
         self.displayed_event.set(self.curr_event)
 
-    def startPlayback(self, data: dict, time_label: tkinter.Label):
+    def startPlayback(self, time_label: tkinter.Label):
         """Replays the captured data (JSON data captured by the plugin) onto displayed_text.
         time_label is used to display the current time, in seconds of the playback from the start. The playback will play while is_playing is set
         and will stop playing when it is cleared."""
         self.curr_time = self.start_time
-        events = data["events"]
+        self.apply_text_event(self.events[0])
         while self.window_open.is_set():
             if self.is_playing.wait(Replay.PAUSE_TIMEOUT):
                 if self.curr_time < self.end_time:
@@ -121,7 +114,6 @@ class Replay:
                     time_label.config(text=(self.curr_time - self.start_time) / Replay.SECONDS_TO_MILLISECONDS)
                     self.displayed_time.set(self.curr_time - self.start_time)
 
-                    self.curr_event = self.next_text_event(self.curr_event)
                     self.displayed_event.set(self.curr_event)
 
                     self.update_text()
@@ -147,24 +139,20 @@ class Replay:
 
         return window
 
-    def last_text_event(self):
-        curr = 0
-        next = self.next_text_event(0)
-        while(next < len(self.events)):
-            curr = next
-            next = self.next_text_event(next + 1)
-        if curr != 0:
-            return curr
-        else:
-            return -1
+    def extract_text_events(self, events):
+        result = [];
+        for event in events:
+            if "time" in event:
+                result.append(event)
+        return result
 
-    def replay_from_file(self, file_name: str = "../../tests/example.json"):
+    def replay_from_file(self, file_name: str = "../../tests/example2.json"):
         with open(file_name) as file:
             file_data = json.loads(file.read())
             session = file_data["sessions"][0]
-            self.events = session["events"]
-            self.start_time = self.events[self.next_text_event(0)]["time"]
-            self.end_time = self.events[self.last_text_event()]["time"]
+            self.events = self.extract_text_events(session["events"])
+            self.start_time = self.events[0]["time"]
+            self.end_time = self.events[len(self.events) - 1]["time"]
             end_time = self.end_time - self.start_time
 
             window = self.createWindow()
@@ -174,7 +162,7 @@ class Replay:
 
             time_slider = tkinter.Scale(window, from_=0, to=end_time, length=600, variable=self.displayed_time, orient="horizontal", command=self.scrub_to_time);
             event_slider = tkinter.Scale(
-                window, from_=0, to=len(self.events), length=600, variable=self.displayed_event, orient="horizontal", command=self.scrub_to_event
+                window, from_=0, to=len(self.events) - 1, length=600, variable=self.displayed_event, orient="horizontal", command=self.scrub_to_event
                 )
 
             time_label = tkinter.Label(text="Not Yet Playing")
@@ -191,7 +179,6 @@ class Replay:
             thread = threading.Thread(
                 target=self.startPlayback,
                 args=(
-                    session,
                     time_label,
                 ),
             )
