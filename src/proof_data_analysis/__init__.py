621816261a4c61b2b1d7acd1b05153276dbff053
trigaten <sanderschulhoff@gmail.com>
2022-12-02 10:42:46 -0500

    feat: easier exports

--- src/proof_data_analysis/__init__.py
+++ src/proof_data_analysis/__init__.py
@@ -1,1 +1,5 @@
+from proof_data_analysis.clustering import *
+from proof_data_analysis.plots import *
+from proof_data_analysis.replay import *
+from proof_data_analysis.statistics import *
 from proof_data_analysis.utils import *
